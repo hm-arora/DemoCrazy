6b8dc0e1f1c15bdc7b93baaac50061b21895e737
anmolduainter <anmolduainter@gmail.com>
2017-09-01 17:58:57 +0530

    Phone Number

--- app/src/main/java/com/example/anmol/democrazy/LoginActivity.java
+++ app/src/main/java/com/example/anmol/democrazy/LoginActivity.java
@@ -3,18 +3,50 @@ package com.example.anmol.democrazy;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by anmol on 1/9/17.
- */
+import com.example.anmol.democrazy.login.PhoneNumber;
+
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button Submit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login_activity);
+        initValues();
+
+        SubmitClick();
+
 
     }
+
+    public void initValues(){
+        editText= (EditText) findViewById(R.id.PhoneNumber);
+        Submit= (Button) findViewById(R.id.SubmitPhoneNumber);
+    }
+
+    public void SubmitClick(){
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 String number=editText.getText().toString();
+
+                PhoneNumber phoneNumber=new PhoneNumber(number,LoginActivity.this);
+                phoneNumber.sendNumber();
+
+            }
+        });
+
+    }
+
 }
---
+++ app/src/main/java/com/example/anmol/democrazy/login/PhoneNumber.java
@@ -0,0 +1,70 @@
+package com.example.anmol.democrazy.login;
+
+
+// Sending Phone Number
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.loopj.android.http.RequestParams;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneNumber {
+
+    Context ctx;
+    RequestQueue rq;
+    String phoneNumber;
+
+    private static final String URL="";
+
+    public PhoneNumber(String phoneNumber,Context ctx){
+
+        this.phoneNumber=phoneNumber;
+        this.ctx=ctx;
+
+    }
+
+    public void sendNumber(){
+
+        rq=Volley.newRequestQueue(ctx);
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        System.out.println(response);
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String,String> map=new HashMap<String,String>();
+                map.put("phone",phoneNumber);
+                return  map;
+            }
+        };
+
+        rq.add(stringRequest);
+
+    }
+
+
+
+}
