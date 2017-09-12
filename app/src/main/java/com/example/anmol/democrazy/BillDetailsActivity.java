323e9df8d497147e59ebca7b0afe8c99906a0f2e
anmolduainter <anmolduainter@gmail.com>
2017-09-12 12:23:26 +0530

    Bills

--- app/src/main/java/com/example/anmol/democrazy/BillDetailsActivity.java
+++ app/src/main/java/com/example/anmol/democrazy/BillDetailsActivity.java
@@ -1,9 +1,21 @@
 package com.example.anmol.democrazy;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.webkit.URLUtil;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.anmol.democrazy.BillsData.BillDetails;
@@ -11,21 +23,22 @@ import com.example.anmol.democrazy.BillsData.BillDetails;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * Created by anmol on 12/9/17.
- */
+import java.io.File;
+
 
 public class BillDetailsActivity extends AppCompatActivity {
 
 
-    TextView BillName,BillDate,SynopContent,Pros,ProsContent,Cons,ConsContent;
+    TextView BillName,BillDate,SynopContent,Pros,ProsContent,Cons,ConsContent,NewspaperLink;
 
+    Button ActualBillLinkButt;
 
     String billName;
     String billDate;
     String synopContent;
     String prosContent;
     String consContent;
+    String newspaperlink;
     String ActualBillLink;
 
     @Override
@@ -34,6 +47,7 @@ public class BillDetailsActivity extends AppCompatActivity {
 
         setContentView(R.layout.bill_details);
 
+        ActualBillLinkButt= (Button) findViewById(R.id.ActualBillLink);
         BillName= (TextView) findViewById(R.id.BillName);
         BillDate= (TextView) findViewById(R.id.BillDate);
         SynopContent= (TextView) findViewById(R.id.SynopContents);
@@ -41,6 +55,7 @@ public class BillDetailsActivity extends AppCompatActivity {
         ProsContent= (TextView) findViewById(R.id.ProsTextCont);
         Cons= (TextView) findViewById(R.id.ConsText);
         ConsContent= (TextView) findViewById(R.id.ConsTextCont);
+        NewspaperLink= (TextView) findViewById(R.id.NewspaperLink);
 
         //getting id of bill
         int id=getIntent().getExtras().getInt("id");
@@ -49,6 +64,7 @@ public class BillDetailsActivity extends AppCompatActivity {
         BillDetails billDetails=new BillDetails(BillDetailsActivity.this,id);
 
         billDetails.getBillD(new BillDetails.BillsCallBack() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void getBillDetails(JSONObject jsonObject) throws JSONException {
 
@@ -62,6 +78,10 @@ public class BillDetailsActivity extends AppCompatActivity {
                     synopContent=jsonObject1.getString("synopsis").replace("\n","\n\n");
                     prosContent=jsonObject1.getString("pros").replace("\n","\n\n ->");
                     consContent=jsonObject1.getString("cons").replace("\n","\n\n ->");
+
+                    newspaperlink=jsonObject1.getString("newspaper_articles_links");
+
+                    ActualBillLink=jsonObject1.getString("actual_bill_link");
 
                     BillName.setText(billName);
                     BillDate.setText(billDate);
@@ -87,8 +107,54 @@ public class BillDetailsActivity extends AppCompatActivity {
                         Cons.setText("Cons");
                         ConsContent.setText(consContent);
                     }
+
+                    //Taking empty string
+                    String s1="";
+                    //Newspaper-Links
+                    String[] s=newspaperlink.split("\n");
+                    for (int i=0;i<s.length;i++){
+                          System.out.println(s[i]);
+                         s1+="<a href="+s[i]+">"+"NewspaperArticle Link-"+(i+1)+"</a><br><br>";
+                    }
+
+                    NewspaperLink.setMovementMethod(LinkMovementMethod.getInstance());
+                    NewspaperLink.setText(Html.fromHtml(s1,0));
+                    NewspaperLink.setLinkTextColor(Color.BLUE);
+
                 }
             }
         });
+
+        ActualBillLinkButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    DownloadBill();
+            }
+        });
+    }
+
+
+    // Downloading PDF
+    public void DownloadBill(){
+
+        String href="http://139.59.86.83:4000"+ActualBillLink;
+
+        File direct=new File(Environment.getExternalStorageDirectory() + "/Democrazy");
+
+        // MAking Directory if not exists
+        if (!direct.exists()){
+            direct.mkdir();
+        }
+
+        DownloadManager.Request request=new DownloadManager.Request(Uri.parse(href));
+        request.setTitle("Downloading pdf");
+        request.setDescription("Bill : "+billName);
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        String filename = URLUtil.guessFileName(href, null, MimeTypeMap.getFileExtensionFromUrl(href));
+        request.setDestinationInExternalPublicDir("/Democrazy", filename);
+        DownloadManager manager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+
     }
 }
