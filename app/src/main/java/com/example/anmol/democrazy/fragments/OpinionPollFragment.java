0d44142357c40dff5e949d91c784416281605922
anmolduainter <anmolduainter@gmail.com>
2017-09-27 11:43:13 +0530

    .

--- app/src/main/java/com/example/anmol/democrazy/fragments/OpinionPollFragment.java
+++ app/src/main/java/com/example/anmol/democrazy/fragments/OpinionPollFragment.java
@@ -15,13 +15,22 @@ import android.widget.TextView;
 import com.example.anmol.democrazy.R;
 import com.example.anmol.democrazy.opinion.OpinionPoll;
 
+import java.util.HashMap;
+
 
 public class OpinionPollFragment extends Fragment {
     private static final String POLL_KEY = "POLL_KEY";
+
+    // Persisting the value of Poll
+    public static HashMap<String,String> hashMap=new HashMap<>();
+
     private static final String TAG = OpinionPollFragment.class.getSimpleName();
     Button btn1, btn2, btn3,mShowButton;
     OpinionPoll mOpinionPoll;
     private boolean isShowButton;
+
+    // Opinion Poll Activity
+    private String id;
 
     /**
      * Used to initialize OpinionPollFragment object
@@ -45,7 +54,7 @@ public class OpinionPollFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.opinion_poll_fragment, container, false);
         mOpinionPoll = (OpinionPoll) getArguments().getSerializable(POLL_KEY);
-        String id = mOpinionPoll.getID();
+        id = mOpinionPoll.getID();
         String question = mOpinionPoll.getQuestion();
         String stateCentralId = mOpinionPoll.getStateCentralID();
         String startDate = mOpinionPoll.getStartDate();
@@ -57,9 +66,32 @@ public class OpinionPollFragment extends Fragment {
         btn3 = (Button) view.findViewById(R.id.button3);
         mShowButton = (Button) view.findViewById(R.id.submitButton);
 
-        btn1.setOnTouchListener(listener);
-        btn2.setOnTouchListener(listener);
-        btn3.setOnTouchListener(listener);
+
+        // Yes Clicking
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickingPoll(1);
+            }
+        });
+
+        //No Clicking
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickingPoll(2);
+            }
+        });
+
+
+        // Dont Know Cliking
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickingPoll(3);
+            }
+        });
+
         descTextView.setText(question);
         descTextView.setTypeface(null, Typeface.BOLD);
 
@@ -72,45 +104,54 @@ public class OpinionPollFragment extends Fragment {
     }
 
 
+    // Clicking Submit Poll Button
     View.OnClickListener mShowButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
-        }
-    };
-    private void checkValue(int value) {
-        if (value == 1) {
-            btn1.setPressed(true);
-            btn2.setPressed(false);
-            btn3.setPressed(false);
-        } else if (value == 2) {
-            btn2.setPressed(true);
-            btn1.setPressed(false);
-            btn3.setPressed(false);
-        } else {
-            btn3.setPressed(true);
-            btn1.setPressed(false);
-            btn2.setPressed(false);
-        }
-    }
 
-    View.OnTouchListener listener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            int value = 0;
-            switch (v.getId()) {
-                case R.id.button1:
-                    value = 1;
-                    break;
-                case R.id.button2:
-                    value = 2;
-                    break;
-                case R.id.button3:
-                    value = 3;
-                    break;
-            }
-            checkValue(value);
-            return true;
+            System.out.println(hashMap);
+
         }
     };
+
+
+    // Cliking Poll
+    void ClickingPoll(int i){
+
+        switch (i){
+
+            // yes
+            case 1:
+                btn1.setPressed(true);
+                btn2.setPressed(false);
+                btn3.setPressed(false);
+
+                hashMap.put(id,"1");
+
+                break;
+
+            // No
+            case 2:
+
+                btn2.setPressed(true);
+                btn1.setPressed(false);
+                btn3.setPressed(false);
+
+                hashMap.put(id,"0");
+
+                break;
+
+            // Dont Know
+            case 3:
+                btn3.setPressed(true);
+                btn1.setPressed(false);
+                btn2.setPressed(false);
+
+                hashMap.put(id,"2");
+
+                break;
+        }
+
+    }
 }
